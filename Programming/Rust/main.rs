49179d9c3a2b4f7c34e1054a187c553fab4ec828
fhanagan1s@semo.edu <fhanagan1s@semo.edu>
2023-02-16 22:24:36 -0700

    Update main.rs

--- Programming/Rust/main.rs
+++ Programming/Rust/main.rs
@@ -1,38 +1,15 @@
-/////////////////////////////////////////////////
-//////////////////////////////////////////////////
-//////////////////////////////////////////////////
-extern crate glutin_window;
-extern crate graphics;
-extern crate opengl_graphics;
-extern crate piston;
 
-extern crate rand;
-
-use piston::window::WindowSettings;
-use piston::event_loop::*;
-use piston::input::*;
-use glutin_window::GlutinWindow;
-use opengl_graphics::{GlGraphics, OpenGL};
-
-use std::collections::LinkedList;
-use std::iter::FromIterator;
-
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-
-use std::io;
-use std::io::Read;
 use std::fs::File;
+extern crate piston_window;
+extern crate find_folder;
+
 use std::io::{BufRead, BufReader};
 use std::fs::OpenOptions;
 use std::io::{BufWriter, Write};
 use std::path::Path;
-// use std::io::prelude::*;
 use std::cmp::Ordering;
 
-
-//fn main() {
+use piston_window::*;
 
 #[derive(Default, Debug)]
 struct Scor {
@@ -41,64 +18,94 @@ struct Scor {
 }
 
 
+//////////////////////////////////////////////////////////////////
+// display_table                                                // 
+//////////////////////////////////////////////////////////////////
+fn  display_table(my_array: &[&str]) {
+    
+    let mut window: PistonWindow = WindowSettings::new(
+            "High Scores Table",
+            [400, 550]
+        )
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+ 
+    let assets = find_folder::Search::ParentsThenKids(3, 3)
+        .for_folder("assets").unwrap();
+       
+    let mut glyphs = window.load_font(assets.join("FiraSans-Bold.ttf")).unwrap();
+    
+    window.set_lazy(true);
+    
+    while let Some(e) = window.next() {
+    
+        window.draw_2d(&e, |c, g, device| {
+      
+         for inx in 0..10 {
 
+           let transform = c.transform.trans(50.0, 50.0 + 50.0 * (inx as f64));
 
-
-//////////////////////////////////////////////////////////////
-//let mut v: Vec<Scor> = Vec::new();
- // let mut v: Vec<Scor> = Vec::new();
-////////////////////////////////////////////////////////////////////////////
- fn create_data_file(s: &String) {
-   // let int_var:i32 = 108;
-   // let str_var = int_var.to_string();
-///////////////////////////////////////////////////////////////////////////////////////////////
-    let file = std::fs::File::create(s).expect("create failed");
-   }
-///////////////////////////////////////////////////////////////////////////
-
-/*
-fn read_data_file(s: &String) {
-    let mut data = String::new();
-   // let mut f = File::open("data.txt").expect("Unable to open file");
-    let mut f = File::open(s).expect("Unable to open file");
-    f.read_to_string(&mut data).expect("Unable to read string");
-    println!("read: {}", data);
+            text::Text::new_color([0.0, 1.0, 0.0, 1.0], 32).draw(
+                my_array[inx],              
+                &mut glyphs,
+                &c.draw_state,
+                transform, g
+            ).unwrap();
+           
+          }   
+            // Update glyphs before rendering.
+            glyphs.factory.encoder.flush(device);
+        });
     }
-*/
- ///////////////////////////////////////////////////////////////////////////
+}
 
-fn read_data_buffered(s: &String) {
- //////////////////////////////////////////////
-    let mut v: Vec<Scor> = Vec::new();
-//////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////
+// create_data_files                                            // 
+//////////////////////////////////////////////////////////////////
+
+ fn create_data_file(s: &String) {  
+   
+    let _file = std::fs::File::create(s).expect("create failed");       
+}
+   
+//////////////////////////////////////////////////////////////////
+// read_data_buffered                                           // 
+//////////////////////////////////////////////////////////////////
+
+fn read_data_buffered(s: &String) {  
+
+    let mut v: Vec<Scor> = Vec::new();   
+
     let f = File::open(s).expect("Unable to open file");
-
+    
     let f = BufReader::new(f);
 
     let mut i:i32 =0;
-
+   
     let mut lxn = 0;
 
     for line in f.lines() {
-
-
+       
+           
         let line = line.expect("Unable to read line");
         i+=1;
-
+        
           if i%2 == 1 {
-
+      
             let m_i: i32 = line.parse().unwrap();
-
-            lxn = m_i;
+           
+            lxn = m_i;                
         } else {
-
+                
           let my_init = line;
-
+         
              v.push(Scor {
                   y: my_init.into(),
                   x: lxn,
-          });
-       }
+          });    
+       }   
     }
 
     v.sort_by(|a, b| {
@@ -111,129 +118,118 @@ fn read_data_buffered(s: &String) {
         }
     });
 
-    if v.len() > 10 {
+    if v.len() > 10 { 
         let i_remove = v.len() - 10;
      //   println!("remove =  { }",i_remove);
 
-        for n in 0..i_remove{
+        for _n in 0..i_remove{
             v.pop();
         }
      }
 
+     
+    let mut my_array = ["--","--", "--", "--", "--", "--", "--", "--", "--", "--"];
+     
+  //   println!("{:?}", my_array);
 
+       
 
-     for n in 0..v.len()  {
-     println!("{ }  { }",v[n].x,v[n].y);
+/*
+     let binding = 0; 
+
+     for ix in 0..v.len() {
+
+        let binding = v[ix].x.to_string() + "  ----->  " +  &v[ix].y;
+
+        my_array[ix] = &binding;
      }
+*/
+   /*  
+     let icount = 0;
+     
+   //  let binding = " ";
+     
+     let binding = v[icount].x.to_string() + "  ----->   " +  &v[icount].y;
+     
+     my_array[icount] = &binding;
+
+     
+     for icn in 0..v.len() {
+        
+         let binding = v[icn].x.to_string() + "  ----->   " +  &v[icn].y;
+
+         my_array[icn] = &binding;   
+     }
+   
+     */
 
      println!("Vector length: {}", v.len());
 
-  //   println!("{ }  { }",v[v.len()-1].x,v[v.len()-1].y);
-
+     
+      
      let f =  File::create(s).expect("File open failed");
 
      let mut f = BufWriter::new(f);
+     
 
-
-     ////////////////////////////////////////////////////////////////////////////////////////////
-     for ix in 0..v.len(){
+     
+     for ix in 0..v.len() {
+      
+      // my_array[ix] = v[ix].x.to_string() + "  ----->   " +  &v[ix].y; 
        f.write_all(v[ix].x.to_string().as_bytes()).expect("Unable to write data");
        f.write_all("\n".to_string().as_bytes()).expect("Unable to write data");
        f.write_all(v[ix].y.to_string().as_bytes()).expect("Unable to write data");
-       f.write_all("\n".to_string().as_bytes()).expect("Unable to write data");
+       f.write_all("\n".to_string().as_bytes()).expect("Unable to write data");     
      }
-}
- ////////////////////////////////////////////////////////////////////////////////
+ 
+       display_table(&my_array);
+
+}           
+ 
+//////////////////////////////////////////////////////////////////
+// append_data_buffered                                         // 
+//////////////////////////////////////////////////////////////////
 
 fn append_data_buffered(s: &String,i: &i32, u_init: &String) {
 
-
+  
     let f = OpenOptions::new()
         .append(true)
         .open(s)
         .expect("Unable to open file");
 
     let mut f = BufWriter::new(f);
-
-    f.write_all(i.to_string().as_bytes()).expect("Unable to write data");
+ 
+    f.write_all(i.to_string().as_bytes()).expect("Unable to write data");   
     f.write_all("\n".to_string().as_bytes()).expect("Unable to write data");
     f.write_all(u_init.to_string().as_bytes()).expect("Unable to write data");
-    f.write_all("\n".to_string().as_bytes()).expect("Unable to write data");
+    f.write_all("\n".to_string().as_bytes()).expect("Unable to write data"); 
 }
 
- ///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+//          main routine                                                   //
+/////////////////////////////////////////////////////////////////////////////
 fn main() {
 
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-const COLS: u32 = 30;
-    const ROWS: u32 = 20;
-    const SQUARE_WIDTH: u32 = 20;
-
-    let width = COLS * SQUARE_WIDTH;
-    let height= ROWS * SQUARE_WIDTH;
-
-
-
- let opengl = OpenGL::V3_2;
-
-    let mut window: GlutinWindow = WindowSettings::new("High Scores:", [width, height])
-        .opengl(opengl)
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
-
-///////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////
-
-
-
  let s = "data.txt".to_string();
-
-// let ind:i32 = 0;
-
-//read in the score
-
-let mut input_text = String::new();
- /*
-io::stdin()
-.read_line(&mut input_text)
-.expect("failed to read from stdin");
-
-let trimmed = input_text.trim();
-match trimmed.parse::<i32>() {
-Ok(ind) => println!("your integer input: {}", ind),
-Err(..) => println!("this was not an integer: {}", trimmed),
- }
-*/
-
-  let mut rs:bool=true;
-
-    rs = Path::new("data.txt").exists();
-
+  
+ let  rs = Path::new("data.txt").exists();
+    
     if rs == true{
         println!("File exists");
     }
     else{
         println!("File does not exist");
-        create_data_file(&s);
-    }
+        create_data_file(&s); 
+    }  
 
+let  _ui = "FH".to_string();
+let   _ik = "jj".to_string();
+ let dd = "DD".to_string(); 
 
+let md = 13;
+ append_data_buffered(&s,&md,&dd);
 
-let  ui = "FH".to_string();
-let  ik = "jj".to_string();
- let dd = "DD".to_string();
-
-
-
-let md = 4;
- append_data_buffered(&s,&md,&ik);
-
-
-//////////////////////////////////////
 read_data_buffered(&s);
 
 }
